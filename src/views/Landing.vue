efc17676dc464f0c418e9a9f371e9bf38405d9c8
HironoriSato <maza.hs5360@gmail.com>
2019-09-28 13:05:37 +0900

    [add]Landing, Signin, Signup画面を新規作成

---
+++ src/views/Landing.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col class="text-center">
+        <h1>Landing page</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
