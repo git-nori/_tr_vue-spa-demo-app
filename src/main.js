0c3adf4cccea5f985d086e515171b7a1a7f00b00
HironoriSato <maza.hs5360@gmail.com>
2019-09-29 23:53:31 +0900

    Firebase SDKをセットアップ npm install --save firebase

--- src/main.js
+++ src/main.js
@@ -3,8 +3,23 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAPqmtJ_Nf5ipoCqXCD-vOxn96rwRfOAD4",
+  authDomain: "fir-app-e8ff2.firebaseapp.com",
+  databaseURL: "https://fir-app-e8ff2.firebaseio.com",
+  projectId: "fir-app-e8ff2",
+  storageBucket: "",
+  messagingSenderId: "61582338246",
+  appId: "1:61582338246:web:7dc7d4d5627dbfce2a0fc7",
+  measurementId: "G-RBWSF2FL0G"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
