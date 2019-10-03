79145bd4a3c224e2bcc0508d1d488aa92146e7b9
HironoriSato <maza.hs5360@gmail.com>
2019-10-03 20:20:27 +0900

    beforeEachを用いてルートガードが実装できずに止まっている

--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import firebase from 'firebase'
 
 // pathと対応するcomponentを格納したmap
 const routerOptions = [
   { path: '/', component: 'Landing' },
-  { path: '/home', component: 'Home' },
+  { path: '/home', component: 'Home', meta: { requiresAuth: true } }, // 認証済みのユーザーしかアクセスできないよう設定
   { path: '/signup', component: 'Signup' },
   { path: '/signin', component: 'Signin' },
 ]
@@ -29,8 +30,23 @@ const routes = routerOptions.map(route => {
 
 Vue.use(Router)
 
-export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
+const router = new Router({
+  moed: 'history',
   routes
 })
+
+// beforeEach => 全てのルートの遷移前、コンポーネントガードの解決前に処理をかける
+router.beforeEach((to, from, next) => {
+  // some()メソッド => 配列(to.matched)の中から真になる要素を返す
+  // to => アクセスするURLに合致するルートレコード(routerOptionsの配列内のオブジェクトのコピー)
+  // 画面の遷移先が「meta」情報の「requiresAuth」の値を格納
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  const isAuthenticated = firebase.auth().currentUser
+  if (requiresAuth && !isAuthenticated) {
+    next('/signin')// /signinにリダイレクトをする
+  } else {
+    next()
+  }
+})
+
+export default router
